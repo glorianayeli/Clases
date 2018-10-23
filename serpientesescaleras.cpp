a58588ae65b12cc528c85a9549353d43c06b3181
glorianayeli <42391634+glorianayeli@users.noreply.github.com>
2018-10-23 17:17:16 -0500

    Update serpientesescaleras.cpp

--- serpientesescaleras.cpp
+++ serpientesescaleras.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 #include <time.h>
 int lanzardado();
+void llenarvector(tablero,tam);
 int main()
 {
     srand(time(0));
     int tablero[101];
     int juno=0, jdos=0;
+    llenarvector(tablero,101);
     while(juno<100&&jdos<100)
     {
         juno+=lanzardado();
@@ -28,4 +30,28 @@ int lanzardado()
     srand(time(0));
     dado=srand()%6+1;
     return dado;
+}
+void llenarvector(tablero,tam)
+{
+    int(i=0;i<101;i++)
+    {
+        tablero[i]=0;
+    }
+    tablero[11]=28;
+    tablero[16]=24;
+    tablero[17]=50;
+    tablero[19]=26;
+    tablero[21]=35;
+    tablero[43]=41;
+    tablero[52]=24;
+    tablero[70]=22;
+    tablero[74]=26;
+    tablero[22]=-20;
+    tablero[18]=-12;
+    tablero[36]=-16;
+    tablero[75]=-45;
+    tablero[78]=-29;
+    tablero[83]=-75;
+    tablero[93]=-53;
+    tablero[96]=-27;
 }
